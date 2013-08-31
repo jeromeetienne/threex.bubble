4e58c83771b34f23beaef7401b46f9d65305d4a1
Jerome Etienne <jerome.etienne@gmail.com>
2013-08-31 16:14:43 +0200

    better demo + better docs

--- threex.bubble.js
+++ threex.bubble.js
@@ -4,6 +4,10 @@
  */
 var THREEx	= THREEx	|| {};
 
+/**
+ * create a THREE.Mesh suitable for a bubble
+ * @param {THREE.Texture} textureCube a texture cube to reflect in the bubble
+ */
 THREEx.BubbleMesh	= function(textureCube){
 	var material	= new THREEx.BubbleMaterial(textureCube)
 	var geometry	= new THREE.SphereGeometry(0.5, 32, 16)
@@ -12,9 +16,8 @@ THREEx.BubbleMesh	= function(textureCube){
 }
 
 /**
- * THREEx extension
- * 
- * @constructor
+ * create a THREE.ShaderMaterial suitable for bubble
+ * @param {THREE.Texture} textureCube a texture cube to reflect in the bubble
  */
 THREEx.BubbleMaterial	= function(textureCube){
 	var shader	= THREE.FresnelShader;
